f1147b04df6faa22460a97c26eb18089f4b6d45d
Jonathan Droniak <droniakj1@owls.southernct.edu>
2014-02-14 22:06:19 -0500

    dark theme added
    I was having some problems styling the dropdown menu in the top bar -
    Jeff could you possibly show me how to style that? I've read that it's
    pretty tricky without SASS for some reason. Thanks.

---
+++ dark_theme.php
@@ -0,0 +1,125 @@
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>A100</title>
+	<link rel="stylesheet" type="text/css" href="foundation/css/normalize.css">
+	<link rel="stylesheet" type="text/css" href="foundation/css/foundation.min.css">
+	<link rel="stylesheet" type="text/css" href="foundation/icons/foundation-icons.css">
+  <link rel="stylesheet" type="text/css" href="style2.css">
+	<link rel="stylesheet" type="text/css" href="dark_style.css">
+	<script src="foundation/js/vendor/modernizr.js"></script>
+</head>
+<body>
+<?php 
+  require('header2.php');
+ ?>
+
+<div class='intro' id='home2-intro'>
+
+    <h2>
+     Connecting talented developers 
+     </h2>
+     <h3>with local Connecticut Startups</h3>
+  <!-- <p>We take our Apprentices and give them free trainings, to get them prepared for paid internships with local startup companies eager to work with new talent.</p> -->
+</div>
+
+<div class='row long-row' id='info-home'>
+  <div class='home-about left large-5 medium-12 small-12 column'>
+    <h2>Interested in Hands-on Web Development Experience?</h2>
+    <p>A100 will train you in the top technologies sought after by startup companies, and the best practices of Agile software development methodology. 
+    <br> 
+    <a href="">Learn More</a></p>
+  </div>
+  <div class='home-about right large-5 medium-12 small-12 column'>
+    <h2>Find Local Talent for Your Startup Company</h2>
+    <p>The A100 Program is an exciting opportunity for your company to secure a well-trained software developer as an intern -- and possibly for full-time employment.
+    <br> 
+    <a href="">Learn More</a></p>
+  </div>
+  
+</div>
+<!-- Three-up Content Blocks -->
+ 
+  <!-- <div class="row"> -->
+    <!-- <div class="large-4 medium-4 columns">
+      <img src="/a100/website/img/code.jpg" />
+      <h4>Accelerating Software Careers from Zero to Startup-Ready</h4>
+      <p>We take our Apprentices and give them free trainings, to get them prepared for paid internships with local startup companies eager to work with new talent.</p>
+    </div> -->
+    
+   <!--  <div class="large-6 medium-6 columns">
+      <img src="/a100/website/img/students.jpg" />
+      <h4>Level Up Your Coding Skills</h4>
+      <p>We’ll train you in the top technologies sought after by startup companies, and the best practices of Agile software development methodology. <a href="">Learn More</a></p>
+      
+    </div>
+    
+    <div class="large-6 medium-6 columns">
+      <img src="/a100/website/img/partners.jpg" />
+      <h4>Find talent for your startup</h4>
+      <p>The A100 Program is an exciting opportunity for your company to secure a well-trained software developer as an intern -- and possibly for full-time employment. <a href="">Learn More</a></p> 
+    </div> -->
+  
+    <!-- </div> -->
+    
+<!-- Call to Action Panel -->
+<!-- <div class="row">
+    <div class="large-12 columns">
+    
+      <div class="panel">
+        <h4>Connect with us...</h4>
+        <p>get the latest updates and contact us - we want to hear from you.</p>
+        <div class="row">
+          <div class="large-6 medium-6 columns">
+            
+            <div><a class="twitter-timeline" href="https://twitter.com/search?q=%23tech" data-widget-id="430524256246431744">Tweets about "#tech"</a>
+            </div>
+          </div>
+          <div class="large-3 medium-3 columns">
+          	<p>Join our Meetup Group</p>
+            <a href=""><img id='meetup-logo' src="/a100/website/img/meetup.png"></a>
+            <br><br><br><br>
+		  	<a href=""><img src="/a100/website/img/facebook.png"></a>
+          </div>
+          <div class="large-3 medium-3 columns">
+          	<p>Email: info@indie-soft.com</p>
+		    <p>760 Chapel Street 
+		    	<br/> New Haven, CT 06510 
+		    	<br/><a target='_blank' href="https://www.google.com/maps/preview/place/760+Chapel+St,+New+Haven,+CT+06510/@41.304616,-72.923485,17z/">Map it</a></p>
+          </div>
+        </div>
+      </div>
+      
+    </div>
+  </div> -->
+ 
+  <!-- Footer -->
+  
+  <footer class="row">
+    <div class="large-12 columns" id="foot">
+      <div class="row">
+        <div class="large-12 columns">
+          <ul class="inline-list right" id='footer'>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Students</a></li>
+            <li><a href="#">Partners</a></li>
+            <li><a href="#">Connect</a></li>
+          </ul>
+        </div>
+      </div>
+    </div> 
+  </footer>
+
+</body>
+  <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+  <!-- for twitter feed end -->
+<script type="text/javascript" src='//code.jquery.com/jquery-1.10.2.min.js'></script>
+<script src="foundation/js/vendor/jquery.js"></script>
+<script src="foundation/js/vendor/fastclick.js"></script>
+  <script src="foundation/js/foundation.min.js"></script>
+  <script type="text/javascript">
+	$(document).foundation();
+	</script>
+
+</html>
